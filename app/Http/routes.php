cfacf608ace4f35a0aa77977c81563d96f7f5843
justin <cong.itsoft@gmail.com>
2016-09-13 09:11:23 +0700

    edit routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,123 +28,14 @@ Route::get('/search', [
 	'uses' => 'SearchController@index'
 ]);
 
-
-// Product detail
-Route::get('/san-pham/{id}-{slug}', ['as' => 'product.detail', 'uses' => 'ProductController@getDetail']);
-Route::get('/lich-su-gia/san-pham/{id}-{slug}', ['as' => 'product.price_histories', 'uses' => 'ProductController@getPriceHistories']);
-Route::get('/upload-anh-san-pham-{id}-{slug}', ['as' => 'product.detail.uploadPhoto', 'uses' => 'ProductController@getUploadPhoto']);
-Route::post('/upload-anh-san-pham-{id}-{slug}', 'ProductController@postUploadPhoto');
-
-// Post detail
-Route::get('/tin-tuc.html', ['as' => 'post.index', 'uses' => 'PostController@getIndex']);
-Route::get('/tin-tuc/{catId}-{catSlug}.html', ['as' => 'post.category', 'uses' => 'PostController@getPostCategory']);
-Route::get('/tin-tuc/{catId}-{catSlug}/{id}-{slug}', ['as' => 'post.detail', 'uses' => 'PostController@getDetail']);
-
-// Post amp version
-// Route::get('/amp/post/{id}', ['as' => 'post.amp', 'uses' => 'PostController@getPostDetailAmp']);
-
-// Post category
-
-// Video detail
-Route::get('/video.html', ['as' => 'video.index', 'uses' => 'VideoController@getIndex']);
-Route::get('/video/{id}-{slug}', ['as' => 'video.detail', 'uses' => 'VideoController@getDetail']);
-
-// Raovat
-Route::get('/rao-vat.html', ['as' => 'raovat.index', 'uses' => 'RaovatController@getIndex']);
-Route::get('/rao-vat/tim-kiem/{keyword}.html', ['as' => 'raovat.search', 'uses' => 'RaovatController@search']);
-Route::get('/rao-vat/{id}', ['as' => 'raovat.detail', 'uses' => 'RaovatController@getDetail']);
-
-// Hỏi đáp
-Route::get('/hoi-dap.html', ['as' => 'hoidap.index', 'uses' => 'HoiDapController@getIndex']);
-Route::get('/hoi-dap/tim-kiem/{keyword}.html', ['as' => 'hoidap.search', 'uses' => 'HoiDapController@search']);
-Route::get('/hoi-dap/{id}', ['as' => 'hoidap.detail', 'uses' => 'HoiDapController@getDetail']);
-
-// Cửa hàng bán
-Route::get('/shop.html', ['as' => 'shop.index', 'uses' => 'ShopController@index']);
-Route::get('/shop/{id}', ['as' => 'shop.detail', 'uses' => 'ShopController@show']);
-
-Route::get('/hang-san-xuat/{brandName}', ['as' => 'product.brand', 'uses' => 'BrandController@getProducts']);
-
-// So sánh giá
-Route::get('/so-sanh', ['as' => 'compare', 'uses' => 'CompareController@getIndex']);
-Route::get('/so-sanh/them-san-pham/{id}', ['as' => 'compare.addProduct', 'uses' => 'CompareController@addProduct']);
-Route::get('so-sanh/xoa-sach', ['as' => 'compare.clear', 'uses' => 'CompareController@clear']);
-Route::get('/so-sanh/{productId}/{opponentId}', ['as' => 'product.vs.opponent', 'uses' => 'CompareController@getProductVsOpponent']);
-
-// Sản phẩm mới
-Route::get('/san-pham-moi', ['as' => 'product.newest', 'uses' => 'ProductController@getNewest']);
-
-// Sản phẩm hot
-Route::get('/san-pham-hot', ['as' => 'product.hot', 'uses' => 'ProductController@getHot']);
-
-// Sản phẩm giá rẻ
-Route::get('/san-pham-gia-re', ['as' => 'product.cheap', 'uses' => 'ProductController@getCheap']);
-
-// Chuyển tới nơi báns
-Route::get('/redirect/product/{id}/{priceId}', ['as' => 'redirect', 'uses' => 'RedirectController@getIndex']);
-
 // Tag
 Route::get('/tag/{name}', ['as' => 'tag.index', 'uses' => 'TagController@getIndex']);
 
-// Tag from product detail
-Route::get('/tag/s/{tag}/', ['as' => 'stag.index', 'uses' => 'STagController@index']);
 
-// Điện thoại
-Route::group(['prefix' => '/dien-thoai'], function() {
 
-	$type = 'dien-thoai';
 
-	Route::get('/', ['as' => 'phone.index', 'uses' => function() use ($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type);
-	}]);
 
-	Route::get('/hang-san-xuat/{brandSlug}', ['as' => 'phone.brand.index', 'uses' => function($brandSlug) use ($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type, $brandSlug);
-	}]);
 
-});
-
-// Laptop
-Route::group(['prefix' => '/laptop'], function() {
-
-	$type = 'laptop';
-
-	Route::get('/', ['as' => 'laptop.index', 'uses' => function() use ($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type);
-	}]);
-
-	Route::get('/hang-san-xuat/{brandSlug}', ['as' => 'laptop.brand.index', 'uses' => function($brandSlug) use ($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type, $brandSlug);
-	}]);
-});
-
-// Tablet
-Route::group(['prefix' => '/may-tinh-bang'], function() {
-
-	$type = 'may-tinh-bang';
-
-	Route::get('/', ['as' => 'tablet.index', 'uses' => function() use($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type);
-	}]);
-
-	Route::get('/hang-san-xuat/{brandSlug}', ['as' => 'tablet.brand.index', 'uses' => function($brandSlug) use($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type, $brandSlug);
-	}]);
-});
-
-// Máy ảnh
-Route::group(['prefix' => '/may-anh'], function() {
-
-	$type = 'may-anh';
-
-	Route::get('/', ['as' => 'camera.index', 'uses' => function() use($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type);
-	}]);
-
-	Route::get('/hang-san-xuat/{brandSlug}', ['as' => 'camera.brand.index', 'uses' => function($brandSlug) use($type) {
-		return App::make('Nht\Http\Controllers\ProductController')->getByType($type, $brandSlug);
-	}]);
-});
 
 // Sitemap
 Route::get('/sitemap.xml', ['as' => 'sitemap.index', 'uses' => 'SiteMapController@getIndex']);
@@ -170,9 +61,6 @@ Route::group(['prefix' => 'sitemap'], function() {
 	Route::get('/qa.xml', ['as' => 'sitemap.qa.index', 'uses' => 'SiteMapController@qaIndex']);
 	Route::get('/qa-page-{page}.xml', ['as' => 'sitemap.qa.page', 'uses' => 'SiteMapController@qaPage']);
 });
-
-Route::get('website-register', ['as' => 'website-register', 'uses' => 'WebsiteRegisterController@getIndex']);
-Route::post('website-register', 'WebsiteRegisterController@postCreate');
 
 
 // Authentication
@@ -202,13 +90,9 @@ require_once app_path() . '/Http/cron_routes.php';
 
 
 Route::group(['prefix' => 'api'], function() {
-	Route::group(['prefix' => 'product'], function() {
-		Route::post('{productId}/like', 'Api\ProductController@addLike');
-	});
+
 });
 
-// Google analytics api data
-Route::get('/ga/analyze', ['as' => 'ga.analyze', 'uses' => 'GaController@analyze']);
 
 
 // Account
